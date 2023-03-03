fdc4507c51ae46093ae65f5fabb4951f468ed211
Alek-pa-Ext <108926741+Alek-pa-Ext@users.noreply.github.com>
2023-03-03 16:09:21 +0200

    method insert

--- lesson10/DLinkedList.py
+++ lesson10/DLinkedList.py
@@ -33,15 +33,56 @@ class DLinkedList:
             node = node.next
         return length
 
+    def find_position(self, position):
+        num_len = self.length()
+        if position not in range(num_len):
+            return None
+        if position <= num_len / 2:
+            node = self.head
+            now_position = 0
+            counter = 1
+            reverse = False
+        else:
+            node = self.tail
+            now_position = num_len - 1
+            counter = -1
+            reverse = True
+        while now_position != position:
+            node = node.prev if reverse else node.next
+            now_position += counter
+        return node
+
+    def insert(self, value, position=None):
+        if position == None:
+            position = self.length()
+        next_node = self.find_position(position)
+        prev_node = self.find_position(position - 1)
+        new_node = Node(value)
+        new_node.next = next_node
+        new_node.prev = prev_node
+        if next_node:
+            next_node.prev = new_node
+        else:
+            self.tail = new_node
+        if prev_node:
+            prev_node.next = new_node
+        else:
+            self.head = new_node
+
+
+
+
 
 
 class Node:
-    def __init__(self, value = None):
+    def __init__(self, value=None):
         self.value = value
         self.next = None
         self.prev = None
 
 if __name__ == "__main__":
-    l = DLinkedList(random.sample(range(0, 100), 15))
+    #l = DLinkedList(random.sample(range(0, 100), 15))
+    l = DLinkedList([0, 1, 2, 3, 4, 5])
+    l.insert(6)
     l.print_list()
-    print(l.length())
+    #print(l.find_position(1))
